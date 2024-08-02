ed00b5d819d9086e183f7696bd8fcef552b4f0b1
Davi Gledson <davigledson@outlook.com>
2024-08-02 05:49:04 -0300

    setando o usuario  no sessionStorage no javascript

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var SessionStorage = require('node-sessionstorage');
+const sessionStorage = SessionStorage;
+const userDadosStorage = "userDadosStorage";
 class usuario {
 
     buscarPorId(id,callback){
@@ -12,6 +15,7 @@ class usuario {
                         break; // Encontra o usuário e sai do loop
                     }
                 }
+               
             }
             callback(err,dados);
         });
@@ -24,9 +28,12 @@ class usuario {
                 for (let u of usuarios) {
                     if (u.email === email && u.senha === senha) {
                         usuario = u;
+                       
+                        
                         break;
                     }
                 }
+
             }
             callback(err,usuario);
         });
@@ -43,6 +50,12 @@ class usuario {
             }
             callback(err,dados);
         });
+    }
+    buscarNoLocalStorage(callback){
+        const usuario = sessionStorage.getItem(userDadosStorage);
+           
+            callback(null,usuario);
+       
     }
 }
 
